c23ad28e06f8e1c6bd3bfa9ddc477bd0278c8634
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-05-03 11:58:55 -0600

    Corrigiendo errores y creacion del README.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,22 +16,39 @@ form.addEventListener("submit", (e) => {
   engine(username, 0, "Username cannot be blank");
   engine(email, 1, "Email cannot be blank");
   engine(password, 2, "Password cannot be blank");
+
+  // Verifica si todos los campos están válidos
+  if (username.value.trim() !== "" && validateEmail(email.value.trim()) && password.value.trim() !== "") {
+    errorMsg[3].innerHTML = "Form submitted successfully!";
+    errorMsg[3].style.color = "green";
+    }
 });
 
 let engine = (id, serial, message) => {
-  if (id.value.trim() === "") {
-    errorMsg[serial].innerHTML = message;
-    id.style.border = "2px solid red";
+    if (id.value.trim() === "") {
+        // Campo vacío
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "2px solid red";
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    } else if (id.type === 'email' && !validateEmail(id.value.trim())) {
+        // Correo electrónico inválido
+        errorMsg[serial].innerHTML = "Please enter a valid email address";
+        id.style.border = "2px solid red";
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    } else {
+        // Campo válido
+        errorMsg[serial].innerHTML = "";
+        id.style.border = "2px solid green";
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "1";
+    }
+};
 
-    // icons
-    failureIcon[serial].style.opacity = "1";
-    successIcon[serial].style.opacity = "0";
-  } else {
-    errorMsg[serial].innerHTML = "";
-    id.style.border = "2px solid green";
-
-    // icons
-    failureIcon[serial].style.opacity = "0";
-    successIcon[serial].style.opacity = "1";
-  }
+// Función para validar el formato del correo electrónico
+let validateEmail = (email) => {
+    // Utiliza una expresión regular para verificar el formato del correo electrónico
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 };
